cffc3da52443f7f8764c0d0de38a1bba7dab53d6
Diego Marquez <diegom@geoowl.com>
2024-08-24 18:54:13 -0700

    Options and yugioh service DI implementation.

---
+++ Cards.WebScraper/Services/YugiohService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cards.WebScraper.Services
+{
+    public class YugiohService : Abstractions.IYugiohService
+    {
+        public YugiohService(IOptions<Options.AppSettingsOptions> options)
+        {
+            
+        }
+    }
+}
