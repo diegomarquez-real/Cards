d32124168fcc31e6d55101c778829d167f9051b6
Diego Marquez <diegom@geoowl.com>
2024-09-01 17:00:07 -0700

    Setup DI and Options

---
+++ Cards.DbUp/Options/ConnectionStringsOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cards.DbUp.Options
+{
+    public class ConnectionStringsOptions
+    {
+        public string MssqlConnection { get; set; }
+    }
+}
--- Cards.DbUp/Program.cs
+++ Cards.DbUp/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using Cards.DbUp;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+builder.Environment.ContentRootPath = Directory.GetCurrentDirectory();
+builder.Configuration.AddJsonFile("appsettings.json", optional: true);
+builder.Services.AddOptions(builder.Configuration); // Configure settings for IOptions DI.
+builder.Services.AddServices(); // Register services for DI.
+using IHost host = builder.Build();
