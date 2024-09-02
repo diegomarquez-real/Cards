b37269653bdd6094142cf1e659164e6681c66c81
Diego Marquez <diegom@geoowl.com>
2024-09-01 17:40:35 -0700

    Log to File using NLog

--- Cards.DbUp/Program.cs
+++ Cards.DbUp/Program.cs
@@ -1,10 +1,16 @@
 ﻿using Cards.DbUp;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 builder.Environment.ContentRootPath = Directory.GetCurrentDirectory();
-builder.Configuration.AddJsonFile("appsettings.json", optional: true);
+builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+LogManager.Configuration = new NLogLoggingConfiguration(builder.Configuration.GetSection("NLog"));
+builder.Logging.AddNLog(); // Add NLog logging provider.
 builder.Services.AddOptions(builder.Configuration); // Configure settings for IOptions DI.
 builder.Services.AddServices(); // Register services for DI.
 using IHost host = builder.Build();
