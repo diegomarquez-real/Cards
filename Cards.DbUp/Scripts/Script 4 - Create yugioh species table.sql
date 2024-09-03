fe77f236edb6aaa7bc136e72f7856d852758b626
Diego Marquez <diegom@geoowl.com>
2024-09-03 14:27:20 -0700

    create attribute and species table scripts

---
+++ Cards.DbUp/Scripts/Script 4 - Create yugioh species table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE [yugioh].[Species](
+	[SpeciesId] [uniqueidentifier] NOT NULL,
+	[Name] [nvarchar](25) NOT NULL,
+ CONSTRAINT [Species_pk] PRIMARY KEY CLUSTERED 
+(
+	[SpeciesId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [yugioh].[Species] ADD  DEFAULT (newid()) FOR [SpeciesId]
+GO
