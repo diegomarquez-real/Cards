2553ef59bf054a480733179ec13be50fb80afce9
Diego Marquez <diegom@geoowl.com>
2024-09-03 17:26:40 -0700

    Create yugioh effectType table

---
+++ Cards.DbUp/Scripts/Script 7 - Create yugioh effectType table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE [yugioh].[EffectType](
+	[EffectTypeId] [uniqueidentifier] NOT NULL,
+	[Name] [nvarchar](25) NOT NULL,
+ CONSTRAINT [EffectType_pk] PRIMARY KEY CLUSTERED 
+(
+	[EffectTypeId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [yugioh].[EffectType] ADD  DEFAULT (newid()) FOR [EffectTypeId]
+GO
