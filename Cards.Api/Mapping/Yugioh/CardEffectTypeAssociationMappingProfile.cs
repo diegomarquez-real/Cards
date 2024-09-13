5293bfc50b7fab33b21e1c170ff90d9a8cf1c10e
Diego Marquez <diegom@geoowl.com>
2024-09-12 19:23:02 -0700

    CardEffectTypeAssociation complete!

---
+++ Cards.Api/Mapping/Yugioh/CardEffectTypeAssociationMappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace Cards.Api.Mapping.Yugioh
+{
+    public class CardEffectTypeAssociationMappingProfile : Profile
+    {
+        public CardEffectTypeAssociationMappingProfile()
+        {
+            CreateMap<Data.Models.CardEffectTypeAssociation, Models.Yugioh.CardEffectTypeAssociationModel>();
+            CreateMap<Models.Yugioh.Create.CreateCardEffectTypeAssociationModel, Data.Models.CardEffectTypeAssociation>();
+        }
+    }
+}
---
+++ Cards.Api/Services/Yugioh/CardEffectTypeAssociationService.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+
+namespace Cards.Api.Services.Yugioh
+{
+    public class CardEffectTypeAssociationService : Abstractions.ICardEffectTypeAssociationService
+    {
+        private readonly IMapper _mapper;
+        private readonly Data.Abstractions.Repositories.Yugioh.ICardEffectTypeAssociationRepository _cardEffectTypeAssociationRepository;
+
+        public CardEffectTypeAssociationService(IMapper mapper,
+            Data.Abstractions.Repositories.Yugioh.ICardEffectTypeAssociationRepository cardEffectTypeAssociationRepository)
+        {
+            _mapper = mapper;
+            _cardEffectTypeAssociationRepository = cardEffectTypeAssociationRepository;
+        }
+
+        public async Task<Models.Yugioh.CardEffectTypeAssociationModel> GetCardEffectTypeAssociationAsync(Guid cardEffectTypeAssociationId)
+        {
+            var cardEffectTypeAssociation = await _cardEffectTypeAssociationRepository.FindByIdAsync(cardEffectTypeAssociationId);
+
+            return _mapper.Map<Models.Yugioh.CardEffectTypeAssociationModel>(cardEffectTypeAssociation);
+        }
+
+        public async Task<Guid> CreateCardEffectTypeAssociationAsync(Models.Yugioh.Create.CreateCardEffectTypeAssociationModel createCardEffectTypeAssociationModel)
+        {
+            var cardEffectTypeAssociation = _mapper.Map<Data.Models.CardEffectTypeAssociation>(createCardEffectTypeAssociationModel);
+            var result = await _cardEffectTypeAssociationRepository.CreateAsync(cardEffectTypeAssociation);
+
+            return result.CardEffectTypeAssociationId;
+        }
+
+        public async Task DeleteCardEffectTypeAssociationAsync(Guid cardEffectTypeAssociationId)
+        {
+            await _cardEffectTypeAssociationRepository.DeleteAsync(cardEffectTypeAssociationId);
+        }
+    }
+}
