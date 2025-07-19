ab76154f49d40d6775af6340ca228b06b69af008
Diego Marquez <diegom@geoowl.com>
2025-07-19 08:48:43 -0700

    custom colors to the blazor sidebar

---
+++ Cards.BlazorServer/Components/Layout/NavMenu.razor.cs
@@ -0,0 +1,28 @@
+﻿using BlazorBootstrap;
+
+namespace Cards.BlazorServer.Components.Layout
+{
+    public partial class NavMenu
+    {
+        IEnumerable<NavItem>? navItems;
+
+        private async Task<Sidebar2DataProviderResult> LoadNavMenu(Sidebar2DataProviderRequest request)
+        {
+            if (navItems?.Any() != true)
+            {
+                navItems = GetNavItems();
+            }
+            return await Task.FromResult(request.ApplyTo(navItems));
+        }
+
+        private IEnumerable<NavItem> GetNavItems()
+        {
+            navItems = new List<NavItem>
+            {
+                new NavItem { Id = "1", Href = "/", IconName = IconName.HouseDoorFill, Text = "Home", },
+                new NavItem { Id = "2", Href = "/yugioh", IconName = IconName.CardImage, Text = "Yugioh", }
+            };
+            return navItems;
+        }
+    }
+}
