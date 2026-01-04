e26a5f9207bcfd709d4b1f8e05ff8f31e06860da
Diego Marquez <diegomnet1@gmail.com>
2026-01-03 18:08:36 -0800

    Successfully get cards based on query model.

--- Cards.BlazorServer/Components/Pages/Yugioh/CardDatabase/Index.razor.cs
+++ Cards.BlazorServer/Components/Pages/Yugioh/CardDatabase/Index.razor.cs
@@ -1,6 +1,30 @@
-﻿namespace Cards.BlazorServer.Components.Pages.Yugioh.CardDatabase
+﻿using Cards.Api.Client.Abstractions.Yugioh;
+using Cards.Api.Models.Yugioh;
+using Cards.Api.Models.Yugioh.Query;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Cards.BlazorServer.Components.Pages.Yugioh.CardDatabase
 {
     public partial class Index
     {
+        [Inject]
+        ICardClient CardClient { get; set; }
+
+        [SupplyParameterFromForm]
+        public InputModel Input { get; set; } = new();
+
+        private async Task SearchAsync()
+        {
+            List<CardModel> cards = await this.CardClient.GetAllOrQueryAsync(new CardQueryModel()
+            {
+                NameSearchText = Input.NameSearchText
+            });
+        }
+
+        public class InputModel
+        {
+            public string NameSearchText { get; set; }
+        }
     }
 }
